708987ab9ab1a085ddd173cca99565c44c98254e
Jonas <lppoweryoshi@gmail.com>
2021-05-04 11:13:36 +0200

    Delete junk

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/main/Program.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/main/Program.java
@@ -2,16 +2,8 @@ package main;
 
 import java.lang.*;
 
-/**
- * Note that Java console applications need to be run through the java runtime
- * by running "java -jar JarFile.jar" in the command line.
- * Java console applications can not be previewed in the Compilr IDE, only applets can.
- */
 public class Program
 {
-    /**
-     * This is the main entry point for the application
-     */
     
     
     public static void main(String args[]) 
